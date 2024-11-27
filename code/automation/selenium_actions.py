e20866aa3c0b8fb42b025a60a2d2d67dce8e83e7
aleromeo <alexromeo160@gmail.com>
2024-11-27 14:20:07 +0100

    fix

--- code/automation/selenium_actions.py
+++ code/automation/selenium_actions.py
@@ -142,7 +142,7 @@ class SeleniumActions:
             submit_button_selector = "[data-testid='tweetButtonInline']"
             # Type the tweet message
             try:
-                sb.add_text(tweet_box_selector, message, timeout=10, retry=1)
+                sb.add_text(tweet_box_selector, message, timeout=10)
             except:
                 return False
 
