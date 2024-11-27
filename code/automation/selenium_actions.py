23250f9de0135d85d6d6fbeec4b56fb17205eb83
aleromeo <alexromeo160@gmail.com>
2024-11-27 14:23:00 +0100

    fix

--- code/automation/selenium_actions.py
+++ code/automation/selenium_actions.py
@@ -147,6 +147,7 @@ class SeleniumActions:
                 return False
 
             self.random_delay(sb)
+            print("Adding emojis")
             self.add_emojis(get_random_emojis())
 
             self.random_delay(sb)
