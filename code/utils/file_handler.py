57225788543f06b012a56cfa6a517c17fd3fb022
aleromeo <alexromeo160@gmail.com>
2024-12-01 22:01:23 +0100

    fix

--- code/utils/file_handler.py
+++ code/utils/file_handler.py
@@ -51,8 +51,7 @@ def get_accounts(number=None):
     accounts = load_json(ACCOUNTS_PATH)
     if number is not None:
         # Get only accounts with the specified number field
-        accounts = [account for account in accounts if account['number'] == str(number)]   
-    print(accounts)    
+        accounts = [account for account in accounts if account['number'] == str(number)] 
     return accounts
 
 def get_account(username):
